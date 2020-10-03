c71c5d0dff1dd9484083a4642030e63685a1f43f
Prakash Kumar Mandal <46490454+Prakash-Mandal@users.noreply.github.com>
2020-10-04 03:04:31 +0530

    Removed DBMS credentials
    Please do not push any credentials with any file. Put spaces or add them to .gitignore file.
    
    Else
    
    1. Push with the file with blank spaces in place of credentials.
    2. Add file to .gitignore
    3. Push .gitignore file

--- Week1/pdo.php
+++ Week1/pdo.php
@@ -6,6 +6,6 @@
  * Time: 8:13 PM
  */
 
-$pdo = new PDO('mysql:host=127.0.0.1;port=8889;dbname=misc', 'root', 'root');
+$pdo = new PDO('mysql:host=127.0.0.1;port=8889;dbname=misc', '', ''); // Do not push credentials.
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
